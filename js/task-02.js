f6e3520bf0ad61fd6f7d9cef999520ebc8afedc8
Polonkoev <103013345+Polonkoev@users.noreply.github.com>
2022-09-12 18:24:29 +0600

    3 task
    Добавлены разные варианты решения задач

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,32 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listEl = document.getElementById("ingredients");
+
+// for (const ingredient of ingredients) {
+//   let element = document.createElement("li");
+//   element.className = "item";
+//   element.innerHTML = ingredient;
+//   listEl.append(element);
+// }
+
+// const createLi = ingredients.map((elem) => {
+//   const liElement = document.createElement("li");
+//   liElement.textContent = elem;
+//   liElement.classList = "item";
+//   return liElement;
+// });
+
+// listEl.append(...createLi);
+
+ingredients.map((elem) => {
+  let element = document.createElement("li");
+  element.className = "item";
+  element.innerHTML = elem;
+  ul.append(element);
+});
--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,44 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+// for (let image of images) {
+
+//   const galleryEl = document.querySelector(".gallery");
+//   const listItem = document.createElement("li");
+//   listItem.classList = "item-position";
+//   const imageItem = `<img src="${image.url}" width = '400' height = '300', alt="${image.alt}">`;
+//   listItem.insertAdjacentHTML("afterbegin", imageItem);
+//   galleryEl.append(listItem);
+// }
+
+// const galleryEl = document.querySelector(".gallery");
+// const imagesEl = images.map(({ url, alt }) => {
+//   const listItem = document.createElement("li");
+//   // listItem.classList = "item-position";
+//   const imageItem = `<img src="${url}" width = '400' height = '300', alt="${alt}">`;
+//   listItem.insertAdjacentHTML("afterbegin", imageItem);
+//   galleryEl.append(listItem);
+// });
+
+const listUl = document.querySelector("ul");
+
+const imgEl = images
+  .map(
+    ({ url, alt }) =>
+      `<li><img src="${url}" width='400' heigth='300', alt="${alt}"></li>`
+  )
+  .join("");
+console.log(imgEl);
+listUl.insertAdjacentHTML("afterbegin", imgEl);
