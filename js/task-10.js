3a2b46f642e0a12baf28b037f7321650917606f3
Polonkoev <103013345+Polonkoev@users.noreply.github.com>
2022-09-21 21:55:24 +0600

    10 task 2 full version

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,5 @@
+// Первый нормальный вариант
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
@@ -5,37 +7,84 @@ function getRandomHexColor() {
 const inputEl = document.querySelector("input");
 const createBoxEl = document.querySelector("button[data-create]");
 const destroyBoxEl = document.querySelector("button[data-destroy]");
-const divEl = document.getElementById("boxes");
+const createDivEl = document.getElementById("boxes");
+let divSize = 20;
 
 function createBoxes(amount) {
-  let createdDiv = [];
-  
-
+  let divsEl = [];
   for (let i = 0; i < amount; i += 1) {
-    createdDiv.push("<div></div>");
+    divSize += 10;
+
+    const createdDiv = document.createElement("div");
+    createdDiv.style.width = `${divSize}px`;
+    createdDiv.style.height = `${divSize}px`;
+    createdDiv.style.backgroundColor = getRandomHexColor();
+
+    divsEl.push(createdDiv);
   }
-  divEl.insertAdjacentHTML("afterbegin", createdDiv.join(""));
-  const boxesChildren = [...divEl.children];
-  const step = 10;
-  let divSize = 30;
-  boxesChildren.map((item) => {
-    item.style.backgroundColor = getRandomHexColor();
-    item.style.width = divSize + "px";
-    item.style.height = divSize + "px";
-    divSize += step;
-  });
+  createDivEl.append(...divsEl);
+ 
 }
-
-function addDiv() {
-  const amount = Number(inputEl.value);
-  createBoxes(amount);
-}
-
-createBoxEl.addEventListener("click", addDiv);
 
 function destroyEl() {
   divEl.innerHTML = "";
   inputEl.value = "";
+  divSize = 20;
 }
 
+function createItem() {
+  createBoxes(inputEl.value);
+}
+
+createBoxEl.addEventListener("click", createItem);
+
 destroyBoxEl.addEventListener("click", destroyEl);
+
+
+
+//Второй вариант не совсем корректный
+
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const inputEl = document.querySelector("input");
+// const createBoxEl = document.querySelector("button[data-create]");
+// const destroyBoxEl = document.querySelector("button[data-destroy]");
+// const divEl = document.getElementById("boxes");
+
+// function createBoxes(amount) {
+//   let createdDiv = [];
+  
+
+//   for (let i = 0; i < amount; i += 1) {
+//     createdDiv.push("<div></div>");
+//   }
+//   divEl.insertAdjacentHTML("afterbegin", createdDiv.join(""));
+//   const boxesChildren = [...divEl.children];
+//   const step = 10;
+//   let divSize = 30;
+//   boxesChildren.map((item) => {
+//     item.style.backgroundColor = getRandomHexColor();
+//     item.style.width = divSize + "px";
+//     item.style.height = divSize + "px";
+//     divSize += step;
+//   });
+// }
+
+// function addDiv() {
+//   const amount = Number(inputEl.value);
+//   createBoxes(amount);
+// }
+
+// createBoxEl.addEventListener("click", addDiv);
+
+// function destroyEl() {
+//   divEl.innerHTML = "";
+//   inputEl.value = "";
+// }
+
+// destroyBoxEl.addEventListener("click", destroyEl);
+
+
